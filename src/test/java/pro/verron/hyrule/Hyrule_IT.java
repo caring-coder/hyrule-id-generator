2358e85db74f67e1f68c5feeb49b581034720387
Joseph Verron <joseph.verron@gmail.com>
2020-11-01 17:47:33 +0100

    extract Hyrule_Test into integration tests

--- src/test/java/pro/verron/hyrule/Hyrule_IT.java
+++ src/test/java/pro/verron/hyrule/Hyrule_IT.java
@@ -12,7 +12,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-public class Hyrule_Tests {
+public class Hyrule_IT {
 
     private Thread thread;
 
